4d24fa03cb283d65ef082bed76019aff58fbaa58
alfianyusufabdullah <alfianyabdullah@gmail.com>
2020-10-03 18:43:33 +0700

    changes all variable nullable

--- app/src/main/java/alfianyusufabdullah/exp/discussion/domain/model/Discussion.kt
+++ app/src/main/java/alfianyusufabdullah/exp/discussion/domain/model/Discussion.kt
@@ -2,11 +2,11 @@ package alfianyusufabdullah.exp.discussion.domain.model
 
 
 data class Discussion(
-    val createdAt: Long,
-    val name: String,
-    val comment: String,
-    val id: Int,
-    val title: String,
-    val reply: Int,
-    val parentId: Int
+    val createdAt: Long? = 0,
+    val name: String? = "-",
+    val comment: String? = "-",
+    val id: Int? = 0,
+    val title: String? = "-",
+    val reply: Int? = 0,
+    val parentId: Int? = 0
 )
