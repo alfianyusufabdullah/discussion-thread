d042ee7cbf2c09417af9f7ced71179777768a9c8
alfianyusufabdullah <alfianyabdullah@gmail.com>
2020-10-03 18:47:43 +0700

    added function to map model to domain

---
+++ app/src/main/java/alfianyusufabdullah/exp/discussion/domain/Mapper.kt
@@ -0,0 +1,18 @@
+package alfianyusufabdullah.exp.discussion.domain
+
+import alfianyusufabdullah.exp.discussion.data.response.DiscussionItemResponse
+import alfianyusufabdullah.exp.discussion.domain.model.Discussion
+
+class Mapper {
+
+    fun discussionDataToDomain(discussionItemResponse: DiscussionItemResponse) =
+        Discussion(
+            id = discussionItemResponse.id,
+            parentId = discussionItemResponse.parentId,
+            title = discussionItemResponse.title,
+            comment = discussionItemResponse.comment,
+            name = discussionItemResponse.name,
+            reply = discussionItemResponse.reply,
+            createdAt = discussionItemResponse.createdAt
+        )
+}
