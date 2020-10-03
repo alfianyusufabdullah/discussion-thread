7db45ed14f5d62a6e09dc8b660c9afc5d26a4397
alfianyusufabdullah <alfianyabdullah@gmail.com>
2020-10-03 17:36:43 +0700

    added network service

---
+++ app/src/main/java/alfianyusufabdullah/exp/discussion/data/route/DiscussionService.kt
@@ -0,0 +1,23 @@
+package alfianyusufabdullah.exp.discussion.data.route
+
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.GET
+import retrofit2.http.POST
+
+interface DiscussionService {
+
+    @FormUrlEncoded
+    @POST("discussion/publish")
+    suspend fun publishNewDiscussionOrComment(
+        @Field("type") type: String = "-",
+        @Field("title") title: String,
+        @Field("comment") comment: String,
+        @Field("name") name: String
+    )
+
+    @GET("discussion/")
+    suspend fun findAllDiscussionWithParent(
+        @Field("parentId") parentId: Int? = null
+    )
+}
