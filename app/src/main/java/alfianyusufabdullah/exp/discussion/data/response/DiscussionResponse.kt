7cbb914db3e97b41bde7a09085c6549f71525639
alfianyusufabdullah <alfianyabdullah@gmail.com>
2020-10-03 17:58:19 +0700

    added message field

--- app/src/main/java/alfianyusufabdullah/exp/discussion/data/response/DiscussionResponse.kt
+++ app/src/main/java/alfianyusufabdullah/exp/discussion/data/response/DiscussionResponse.kt
@@ -2,5 +2,6 @@ package alfianyusufabdullah.exp.discussion.data.response
 
 data class DiscussionResponse(
 	val discussion: List<DiscussionItemResponse> = emptyList(),
-	val status: String? = null
+	val status: String? = null,
+	val message: String? = null
 )
