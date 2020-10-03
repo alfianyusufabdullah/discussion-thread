4d3f39ab94b82deb9e0647b4d7aa39e3d6a782dc
alfianyusufabdullah <alfianyabdullah@gmail.com>
2020-10-03 17:58:08 +0700

    added return value

--- app/src/main/java/alfianyusufabdullah/exp/discussion/data/route/DiscussionService.kt
+++ app/src/main/java/alfianyusufabdullah/exp/discussion/data/route/DiscussionService.kt
@@ -1,5 +1,7 @@
 package alfianyusufabdullah.exp.discussion.data.route
 
+import alfianyusufabdullah.exp.discussion.data.response.DiscussionResponse
+import retrofit2.Response
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
 import retrofit2.http.GET
@@ -14,10 +16,10 @@ interface DiscussionService {
         @Field("title") title: String = "-",
         @Field("comment") comment: String,
         @Field("name") name: String
-    )
+    ): Response<DiscussionResponse>
 
     @GET("discussion/")
     suspend fun findAllDiscussionWithParent(
         @Field("parentId") parentId: Int? = null
-    )
+    ): Response<DiscussionResponse>
 }
