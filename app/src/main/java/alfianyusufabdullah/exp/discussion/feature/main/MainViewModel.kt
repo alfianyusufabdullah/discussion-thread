53888058d503e9ffe968f452fb237b89a79e4190
alfianyusufabdullah <alfianyabdullah@gmail.com>
2020-10-03 20:20:48 +0700

    completed main viewmodel

---
+++ app/src/main/java/alfianyusufabdullah/exp/discussion/feature/main/MainViewModel.kt
@@ -0,0 +1,33 @@
+package alfianyusufabdullah.exp.discussion.feature.main
+
+import alfianyusufabdullah.exp.discussion.domain.model.Discussion
+import alfianyusufabdullah.exp.discussion.domain.usecase.GetAllDiscussionTaskUseCase
+import alfianyusufabdullah.exp.discussion.feature.base.BaseViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.onCompletion
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.launch
+
+@ExperimentalCoroutinesApi
+class MainViewModel(
+    private val allDiscussionTaskUseCase: GetAllDiscussionTaskUseCase
+) : BaseViewModel() {
+
+    private val _discussions = MutableLiveData<List<Discussion>>()
+    val discussions: LiveData<List<Discussion>> = _discussions
+
+    fun findAllDiscussion() {
+        viewModelScope.launch {
+            allDiscussionTaskUseCase.findAllDiscussion()
+                .onStart { loadingLiveData.postValue(true) }
+                .onCompletion { loadingLiveData.postValue(false) }
+                .catch { errorLiveData.postValue(it.message) }
+                .collect { _discussions.postValue(it) }
+        }
+    }
+}
