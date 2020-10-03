5aab200aa6a7b7ba68985a10d0583d34199466b2
alfianyusufabdullah <alfianyabdullah@gmail.com>
2020-10-03 17:53:39 +0700

    Resource

--- app/src/main/java/alfianyusufabdullah/exp/discussion/data/DiscussionRepository.kt
+++ app/src/main/java/alfianyusufabdullah/exp/discussion/data/DiscussionRepository.kt
@@ -1,4 +1,41 @@
 package alfianyusufabdullah.exp.discussion.data
 
-class DiscussionRepository {
+import alfianyusufabdullah.exp.discussion.data.response.DiscussionResponse
+import alfianyusufabdullah.exp.discussion.data.route.DiscussionService
+import alfianyusufabdullah.exp.discussion.domain.model.Discussion
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import java.io.IOException
+
+class DiscussionRepository(private val discussionService: DiscussionService) {
+
+    suspend fun publishNewDiscussion(discussion: Discussion): Flow<DiscussionResponseOutput<DiscussionResponse>> {
+        return flow {
+            val response = discussionService.publishNewDiscussionOrComment(
+                "discussion",
+                discussion.title,
+                discussion.comment,
+                discussion.name
+            )
+
+            if (response.isSuccessful){
+                emit(DiscussionResponseOutput.Success(response.body()))
+            } else {
+                emit(DiscussionResponseOutput.Failure(IOException("")))
+            }
+
+        }
+    }
+
+    suspend fun publishNewComment(discussion: Discussion): Flow<String> {
+
+    }
+
+    suspend fun findAllDiscussion() {
+
+    }
+
+    suspend fun findAllDiscussionWithParentId(parentId: Int) {
+
+    }
 }
