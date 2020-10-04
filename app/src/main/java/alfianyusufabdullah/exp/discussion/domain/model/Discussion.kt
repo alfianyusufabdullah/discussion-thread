e987bae1609ffdfe7eff9ed2cb338354419174f9
alfianyusufabdullah <alfianyabdullah@gmail.com>
2020-10-04 14:59:32 +0700

    changes id and parentId type

--- app/src/main/java/alfianyusufabdullah/exp/discussion/domain/model/Discussion.kt
+++ app/src/main/java/alfianyusufabdullah/exp/discussion/domain/model/Discussion.kt
@@ -1,12 +1,16 @@
 package alfianyusufabdullah.exp.discussion.domain.model
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 
+
+@Parcelize
 data class Discussion(
     val createdAt: Long? = 0,
     val name: String? = "-",
     val comment: String? = "-",
-    val id: Int? = 0,
+    val id: String? = "-",
     val title: String? = "-",
     val reply: Int? = 0,
-    val parentId: Int? = 0
-)
+    val parentId: String? = "-"
+) : Parcelable
