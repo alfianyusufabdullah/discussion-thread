921f913b15b9838abf3b007caa7b4ad70d234483
alfianyusufabdullah <alfianyabdullah@gmail.com>
2020-10-04 15:00:43 +0700

    added function to open thread fragment

--- app/src/main/java/alfianyusufabdullah/exp/discussion/feature/main/MainActivity.kt
+++ app/src/main/java/alfianyusufabdullah/exp/discussion/feature/main/MainActivity.kt
@@ -2,6 +2,8 @@ package alfianyusufabdullah.exp.discussion.feature.main
 
 import alfianyusufabdullah.exp.discussion.R
 import alfianyusufabdullah.exp.discussion.data.Resources
+import alfianyusufabdullah.exp.discussion.domain.model.Discussion
+import alfianyusufabdullah.exp.discussion.feature.thread.ThreadFragment
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -12,8 +14,15 @@ import org.koin.android.viewmodel.ext.android.viewModel
 @ExperimentalCoroutinesApi
 class MainActivity : AppCompatActivity() {
 
-    private val mainAdapter: MainAdapter by lazy { MainAdapter(mutableListOf()) }
+    private val mainAdapter: MainAdapter by lazy { MainAdapter(mutableListOf(), click) }
     private val mainViewModel: MainViewModel by viewModel()
+
+    private val click: (Discussion) -> Unit = {
+        supportFragmentManager.beginTransaction()
+            .add(android.R.id.content, ThreadFragment.newInstance(it))
+            .addToBackStack(null)
+            .commit()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
