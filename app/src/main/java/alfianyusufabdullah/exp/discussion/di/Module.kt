12a2506d197ecdb18d3b59ff311a0d3673b8569e
alfianyusufabdullah <alfianyabdullah@gmail.com>
2020-10-04 15:03:23 +0700

    register thread usecase and viewmodel

--- app/src/main/java/alfianyusufabdullah/exp/discussion/di/Module.kt
+++ app/src/main/java/alfianyusufabdullah/exp/discussion/di/Module.kt
@@ -4,7 +4,9 @@ import alfianyusufabdullah.exp.discussion.data.DiscussionRepository
 import alfianyusufabdullah.exp.discussion.data.route.DiscussionClient
 import alfianyusufabdullah.exp.discussion.domain.Mapper
 import alfianyusufabdullah.exp.discussion.domain.usecase.GetAllDiscussionTaskUseCase
+import alfianyusufabdullah.exp.discussion.domain.usecase.GetAllDiscussionWithParentIdTaskUseCase
 import alfianyusufabdullah.exp.discussion.feature.main.MainViewModel
+import alfianyusufabdullah.exp.discussion.feature.thread.ThreadViewModel
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.koin.android.viewmodel.dsl.viewModel
 import org.koin.dsl.module
@@ -17,9 +19,11 @@ val dataModule = module {
 val domainModule = module {
     single { Mapper() }
     factory { GetAllDiscussionTaskUseCase(get(), get()) }
+    factory { GetAllDiscussionWithParentIdTaskUseCase(get(), get()) }
 }
 
 @ExperimentalCoroutinesApi
 val presentationModule = module {
     viewModel { MainViewModel(get()) }
+    viewModel { ThreadViewModel(get()) }
 }
